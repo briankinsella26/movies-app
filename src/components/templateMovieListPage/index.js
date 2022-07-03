68d3e4586f56a37c11b31f231a5534545e1ea75f
Brian Kinsella <brian.kinsella@distilled.ie>
2022-07-03 16:26:21 +0100

    Using react-query to manage the browser-based cache.

--- src/components/templateMovieListPage/index.js
+++ src/components/templateMovieListPage/index.js
@@ -7,18 +7,18 @@ import Drawer from "@material-ui/core/Drawer";
 import { makeStyles } from "@material-ui/core/styles";
 import MovieList from "../movieList";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) =>  ({
   root: {
     backgroundColor: "#bfbfbf",
     paddingTop: theme.spacing(7),
   },
   fab: {
     marginTop: theme.spacing(8),
-    postition: "fixed",
+    position: "fixed",
     top: theme.spacing(2),
-    right: theme.spacing(2)
+    right: theme.spacing(2),
   },
-}))
+}));
 
 function MovieListPageTemplate({ movies, title, selectFavourite }) {
   const classes = useStyles();
@@ -30,17 +30,16 @@ function MovieListPageTemplate({ movies, title, selectFavourite }) {
 
   let displayedMovies = movies
     .filter((m) => {
-      return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1
+      return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
     })
     .filter((m) => {
       return genreId > 0 ? m.genre_ids.includes(genreId) : true;
     });
 
   const handleChange = (type, value) => {
-    if(type ==="title") setTitleFilter(value);
+    if (type === "title") setTitleFilter(value);
     else setGenreFilter(value);
   };
-
 
   return (
     <>
@@ -53,26 +52,25 @@ function MovieListPageTemplate({ movies, title, selectFavourite }) {
       </Grid>
     </Grid>
     <Fab
-      color="secondary"
-      variant="extended"
-      onClick={() => setDrawerOpen(true)}
-      className={classes.fab}
+        color="secondary"
+        variant="extended"
+        onClick={() => setDrawerOpen(true)}
+        className={classes.fab}
       >
-      Filter
-    </Fab>
-    <Drawer
-      anchor="left"
-      open={drawerOpen}
-      onClose={() => setDrawerOpen(false)}
+        Filter
+      </Fab>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
       >
         <FilterCard
           onUserInput={handleChange}
           titleFilter={titleFilter}
           genreFilter={genreFilter}
-          />
+        />
       </Drawer>
-    </>
+    </>    
   );
 }
-
 export default MovieListPageTemplate;
