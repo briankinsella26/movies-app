a2cc4724441a6de6ab9117baa51c794fedfd36ef
Brian Kinsella <brian.kinsella@distilled.ie>
2022-08-23 18:28:45 +0100

    improvements to login added 2 test users

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,20 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../contexts/authContext";
+import { TextField, Container, Button, Box } from "@material-ui/core/";
 
 const LoginPage = () => {
-  const {authenticate } = useContext(AuthContext);
+  const { authenticate } = useContext(AuthContext);
 
-  const login = () => {
-    const password = Math.random().toString(36).substring(7);
-    authenticate('user1', password);
+  const login = (event) => {
+    event.preventDefault();
+    authenticate(username, password);
   };
+
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
 
   return (
     <>
-      <h2>Login page</h2>
-      <p>You must log in to view the protected pages </p>
-      {/* Login web form  */}
-      <button onClick={login}>Submit</button>
+      <br/>
+      <br/>
+      <br/>
+      <form onSubmit={login}>
+        <Container maxWidth="xs">
+          <h1>Login</h1>
+          <Box mb={1}>
+              <TextField variant="outlined" label="email" fullWidth autoComplete="email" onChange={e => setUserName(e.target.value)}/>
+          </Box>
+          <Box mb={2}>
+              <TextField variant="outlined" label="password" fullWidth autoComplete="password" onChange={e => setPassword(e.target.value)}/>
+          </Box>
+          <Button variant="contained" color="primary" fullWidth type="submit">Log in</Button>
+        </Container>
+      </form>
     </>
     )
 };
